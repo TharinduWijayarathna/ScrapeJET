02aa567857a6766190d0bbb2ba9d25f8b7ac13d0
TharinduWijayarathna <wikum.dev@gmail.com>
2025-08-29 12:08:31 +0530

    chore: initialize core project modules and structure

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+ScrapeJET - Universal Web Scraper with RAG capabilities
+"""
+
+__version__ = "1.0.0"
+__author__ = "ScrapeJET Team"
+__description__ = "Universal Web Scraper with RAG capabilities"
